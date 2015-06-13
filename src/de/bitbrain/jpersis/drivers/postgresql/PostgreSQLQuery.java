d054548b430d2090bf213ca0ef463aa73148bd6f
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-06-14 00:21:31 +0200

    issue #22: initial version of PostgreSQL query class

---
+++ src/de/bitbrain/jpersis/drivers/postgresql/PostgreSQLQuery.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2015 Miguel Gonzalez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.bitbrain.jpersis.drivers.postgresql;
+
+import java.sql.Statement;
+
+import de.bitbrain.jpersis.drivers.jdbc.JDBCQuery;
+import de.bitbrain.jpersis.util.Naming;
+
+public class PostgreSQLQuery extends JDBCQuery {
+
+  public PostgreSQLQuery(Class<?> model, Naming naming, Statement statement) {
+    super(model, naming, statement);
+  }
+
+  @Override
+  protected Slang createSlang() {
+    return new Slang() {
+
+      @Override
+      public String getAutoIncrement() {
+        return "";
+      }
+
+      @Override
+      public String getTypeRangeString() {
+        return "";
+      }
+
+    };
+  }
+
+}
