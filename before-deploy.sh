401b3119c607ec91d4384f91f4f267137907ae3b
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-07-28 18:43:22 +0200

    Fix environment variables

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in signingkey.asc.enc -out signingkey.asc -d
+    openssl aes-256-cbc -K $encrypted_434c6c55d411_key -iv $encrypted_434c6c55d411_iv -in signingkey.asc.enc -out signingkey.asc -d
     gpg --fast-import signingkey.asc
     gpg --export-secret-keys ${env.GPG_KEY_NAME} | gpg --import
 fi
