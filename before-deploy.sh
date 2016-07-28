107d15477d7c6485b4380bb0583a3e0ba703008b
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-07-28 19:02:56 +0200

    Fix signing key file name

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_434c6c55d411_key -iv $encrypted_434c6c55d411_iv -in signingkey.asc.enc -out signingkey.asc -d
-    gpg --fast-import signingkey.asc
-    gpg --export-secret-keys $GPG_KEY_NAME | gpg --import
+    openssl aes-256-cbc -K $encrypted_434c6c55d411_key -iv $encrypted_434c6c55d411_iv -in codesigning.asc.enc -out codesigning.asc -d
+    gpg --fast-import codesigning.asc
 fi
