a6447f7cd6f513f9a605bd6f0e38336948dd0f43
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-07-28 19:07:08 +0200

    Remove debugflag

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-mvn deploy -P sign,build-extras -X --settings settings.xml
+mvn deploy -P sign,build-extras --settings settings.xml
 fi
