d8a9583be1cda065f499c18acb42f2c7d7da25be
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-07-29 08:30:33 +0200

    Proper usage of bash annotation

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc \
         -K $encrypted_434c6c55d411_key \
