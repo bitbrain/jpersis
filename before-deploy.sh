1b2216e9efab423634f9378eff188a1fcd196782
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-07-29 08:29:48 +0200

    Ignore maven tests while deploying

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_434c6c55d411_key -iv $encrypted_434c6c55d411_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import codesigning.asc
+    openssl aes-256-cbc \
+        -K $encrypted_434c6c55d411_key \
+        -iv $encrypted_434c6c55d411_iv \
+        -in codesigning.asc.enc \
+        -out codesigning.asc \
+        -d
+    gpg \
+        --fast-import codesigning.asc
 fi
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-mvn deploy -P sign,build-extras --settings settings.xml
+mvn deploy \
+    -P sign,build-extras \
+    -DskipTests \
+    --settings settings.xml
 fi
