99a64a2b97b995e570d67058b6ec318bf2a85594
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-07-29 10:03:16 +0200

    Modify key encryption

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc \
-        -K $encrypted_434c6c55d411_key \
-        -iv $encrypted_434c6c55d411_iv \
-        -in codesigning.asc.enc \
-        -out codesigning.asc \
-        -d
+    openssl aes-256-cbc -K $encrypted_c791077d38fd_key -iv $encrypted_c791077d38fd_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg \
         --fast-import codesigning.asc
 fi
