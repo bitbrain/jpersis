0135c4d01f8e2461f701638c214ce5d4287ca8db
travis-ci <travis@travis-ci.org>
2016-08-08 22:17:08 +0200

    Add javadoc deployment script (WIP)

---
+++ deployment/deploy-javadoc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Deploying Javadoc to Github pages"
+
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet --branch=master https://${GITHUB_TOKEN}@github.com/MyRealityCoding/jpersis
+mkdir docs
+cd jpersis
+mvn javadoc:javadoc
+cd ..
+mv -Rf jpersis/target/site/apidocs/* docs
+cd jpersis
+git checkout gh-pages
+mv -Rf ../docs .
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER - update Javadocs"
+git push -fq origin gh-pages > /dev/null
